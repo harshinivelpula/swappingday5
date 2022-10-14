d875963294b15d54629583ff83bfff116d0670ff
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-14 20:47:55 +0530

    Swap

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+public class Swap
+{
+    public static void Main(string[] args)
+    {
+        int a = 5, b = 10;
+        Console.WriteLine("Before swap a= " + a + " b= " + b);
+        a = a * b;      
+        b = a / b;      
+        a = a / b;     
+        Console.WriteLine("After swap a= " + a + " b= " + b);
+    }
+}
